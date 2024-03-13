a566c1bac3fd6f7d3e88155dd073b3402c315a26
trrakshitha-reddy <rakshubujji2004@gmail.com>
2024-03-13 07:39:00 +0530

    first commit

---
+++ e-commerce-frontend/src/Components/Hero/Hero.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Hero.css";
+import hero_image from "../Assets/hero_image.png";
+import hand_icon from "../Assets/hand_icon.png";
+import arrow_icon from "../Assets/arrow.png";
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="hero-left">
+        <h2>  </h2>
+        <div>
+          
+          <p>Mangalya</p>
+          <p>Kurthis</p>
+        </div>
+        
+      </div>
+     
+    </div>
+  );
+};
+
+export default Hero;
---
+++ e-commerce-frontend/src/Components/Offers/Offers.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Offers.css";
+import exclusive_image from "../Assets/exclusive_image.png";
+
+const Offers = () => {
+  return (
+    <div className="offers">
+      <div className="offers-left">
+        <h1>Exclusive Offers</h1>
+        <h1>4 Kurtha Tops for 1000/-</h1>
+        <p> </p>
+        <button>Check now</button>
+      </div>
+      
+    </div>
+  );
+};
+
+export default Offers;
